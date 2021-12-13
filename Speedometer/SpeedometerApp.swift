47321ee3f8273c2c5d72d6b468b10dc078022204
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-13 21:43:08 +0530

    Initial Commit

---
+++ Speedometer/SpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerApp.swift
+//  Speedometer
+//
+//  Created by user208584 on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
